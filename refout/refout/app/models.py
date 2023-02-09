ca4a0721621ed166eac04793b7e07d8ccc58bace
Fábio André Neiva Cruz Antunes <a27360>
2023-02-09 16:42:50 +0000

    database change

--- refout/refout/app/models.py
+++ refout/refout/app/models.py
@@ -1,15 +1,44 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
-User = get_user_model()
+
+
 # Create your models here.
 
-class Profile(models.Model):
-    user= models.ForeignKey(User, on_delete=models.CASCADE)
-    id_user = models.IntegerField()
-    bio = models.TextField(blank=True)
-    profileimg= models.ImageField(upload_to='profile_images', default='blank-profile-picutre.jpg')
-    
 
-    def __str__(self):
-        return self.user.username
+
+class Comments(models.Model):
+    comment_date = models.DateField()
+    comment_text = models.CharField(max_length=200)
+    post = models.ForeignKey('Post', models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'comments'
+
+class Post(models.Model):
+    post_date = models.DateField()
+    profile_id = models.IntegerField(unique=True)
+    likes = models.IntegerField()
+    foto = models.CharField(max_length=50)
+    ref = models.ForeignKey('Referencia', models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'Post'
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True,)
+    nome = models.CharField(max_length=50)
+    imagem = models.ImageField(upload_to='profile_images', default='blank-profile-picutre.jpg')
+    seguidores = models.IntegerField()
+    post = models.ForeignKey('Post', models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'profile'
+
+class Referencia(models.Model):
+    post_id = models.IntegerField()
+    ref_descricao = models.CharField(max_length=25)
+
+    class Meta:
+        db_table = 'referencia'
